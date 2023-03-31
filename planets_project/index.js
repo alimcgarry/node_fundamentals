c082b2f7bd53c5c893952aba1fb1bf2d37eb00db
Ali McGarry <ali.mcgarry@gmail.com>
2023-03-31 16:23:16 +0100

    parse kepler_data csv file

--- planets_project/index.js
+++ planets_project/index.js
@@ -1,1 +1,23 @@
 const { parse } = require("csv-parse");
+const fs = require("fs");
+const { resourceLimits } = require("worker_threads");
+
+const results = [];
+
+fs.createReadStream("./kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    results.push(data);
+  })
+  .on("error", (err) => {
+    console.log(err);
+  })
+  .on("end", () => {
+    console.log(results);
+    console.log("done");
+  });
