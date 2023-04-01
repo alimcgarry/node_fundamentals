a5a9fc4f2da54eec8f493955a8cdbd58f576eb89
Ali McGarry <ali.mcgarry@gmail.com>
2023-04-01 21:41:54 +0100

    Get and put requests in vanilla nodejs

--- http_server/index.js
+++ http_server/index.js
@@ -1,12 +1,56 @@
+const { fdatasync } = require("fs");
 const http = require("http");
 
 const PORT = 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Constent-Type": "application/json",
-  });
-  res.end(JSON.stringify({ id: 1, name: "Sir Isaac Newton" }));
+const server = http.createServer();
+
+const friends = [
+  {
+    id: 0,
+    name: "Nikola Tesla",
+  },
+  {
+    id: 1,
+    name: "Albert Einstein",
+  },
+  {
+    id: 2,
+    name: "Isaac Newton",
+  },
+];
+
+server.on("request", (req, res) => {
+  const items = req.url.split("/");
+  if (req.method === "POST" && items[1] === "friends") {
+    req.on("data", (data) => {
+      const friend = data.toString();
+      console.log("Request:", friend);
+      friends.push(JSON.parse(friend));
+    });
+  } else if (req.method === "GET" && items[1] === "friends") {
+    if (items[1] === "friends") {
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      if (items.length === 3) {
+        const friendsIndex = items[2];
+        res.end(JSON.stringify(friends[friendsIndex]));
+      } else {
+        res.end(JSON.stringify(friends));
+      }
+    } else if (req.url === "/messages") {
+      res.setHeader("Content-Type", "text/html");
+      res.write("<html>");
+      res.write("<body>");
+      res.write("<h1>Hello there!</h1>");
+      res.write("</body>");
+      res.write("</html>");
+      res.end();
+    } else {
+      res.statusCode = 404;
+      res.end;
+    }
+  }
 });
 
 server.listen(PORT, () => {
