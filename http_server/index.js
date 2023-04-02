d4da99a084ed0d52892fbd6e7443a80ae9bf64b5
Ali McGarry <ali.mcgarry@gmail.com>
2023-04-02 23:35:38 +0100

    add pipe between request and response

--- http_server/index.js
+++ http_server/index.js
@@ -28,6 +28,7 @@ server.on("request", (req, res) => {
       console.log("Request:", friend);
       friends.push(JSON.parse(friend));
     });
+    req.pipe(res);
   } else if (req.method === "GET" && items[1] === "friends") {
     if (items[1] === "friends") {
       res.statusCode = 200;
